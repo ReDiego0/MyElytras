c83d7a4c1a5d150aba2d16b951daf7a922765246
ReDiego0 <re.diego.maldonado@gmail.com>
2026-01-22 19:16:27 -0300

    chore: Initial setup

---
+++ src/main/kotlin/org/ReDiego0/myElytras/MyElytras.kt
@@ -0,0 +1,14 @@
+package org.ReDiego0.myElytras
+
+import org.bukkit.plugin.java.JavaPlugin
+
+class MyElytras : JavaPlugin() {
+
+    override fun onEnable() {
+        // Plugin startup logic
+    }
+
+    override fun onDisable() {
+        // Plugin shutdown logic
+    }
+}
