4dbb2c8849750715f89ef4db97f1e5b763e0e3ee
ReDiego0 <re.diego.maldonado@gmail.com>
2026-01-22 20:31:15 -0300

    feat: Add ElytraTask for applying custom passive effects

---
+++ src/main/kotlin/org/ReDiego0/myElytras/task/ElytraTask.kt
@@ -0,0 +1,25 @@
+package org.ReDiego0.myElytras.task
+
+import org.ReDiego0.myElytras.config.ElytraConfigLoader
+import org.bukkit.Bukkit
+import org.bukkit.potion.PotionEffect
+import org.bukkit.scheduler.BukkitRunnable
+
+class ElytraTask(private val loader: ElytraConfigLoader) : BukkitRunnable() {
+
+    override fun run() {
+        for (player in Bukkit.getOnlinePlayers()) {
+            val chestplate = player.inventory.chestplate
+
+            if (chestplate == null || chestplate.type.isAir) continue
+
+            // Verificar si es custom
+            val customElytra = loader.getElytraFromItem(chestplate) ?: continue
+
+            // Aplicamos los efectos
+            for ((type, amplifier, ambient) in customElytra.passiveEffects) {
+                player.addPotionEffect(PotionEffect(type, 80, amplifier, ambient, false))
+            }
+        }
+    }
+}
