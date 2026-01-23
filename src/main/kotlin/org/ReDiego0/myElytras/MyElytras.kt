d935d01fc6c62be6cb9b0af8093989eee76ede92
ReDiego0 <re.diego.maldonado@gmail.com>
2026-01-23 00:05:54 -0300

    feat: Integrate ElytraVisualManager and update ElytraTask for enhanced visual effects

--- src/main/kotlin/org/ReDiego0/myElytras/MyElytras.kt
+++ src/main/kotlin/org/ReDiego0/myElytras/MyElytras.kt
@@ -3,29 +3,35 @@ package org.ReDiego0.myElytras
 import org.ReDiego0.myElytras.command.GiveCommand
 import org.ReDiego0.myElytras.config.ElytraConfigLoader
 import org.ReDiego0.myElytras.listener.ElytraMechanicListener
+import org.ReDiego0.myElytras.manager.ElytraVisualManager
 import org.ReDiego0.myElytras.task.ElytraTask
 import org.bukkit.plugin.java.JavaPlugin
 
 class MyElytras : JavaPlugin() {
+
     private lateinit var configLoader: ElytraConfigLoader
+    private lateinit var visualManager: ElytraVisualManager
 
     override fun onEnable() {
         saveDefaultConfig()
-
         configLoader = ElytraConfigLoader(this)
         configLoader.load()
 
-        server.pluginManager.registerEvents(ElytraMechanicListener(configLoader), this)
-        ElytraTask(configLoader).runTaskTimer(this, 0L, 20L)
+        visualManager = ElytraVisualManager(configLoader)
 
         getCommand("myelytra")?.setExecutor(GiveCommand(configLoader))
+        server.pluginManager.registerEvents(ElytraMechanicListener(configLoader), this)
 
-        logger.info("MyElytras ha sido habilitado con éxito (≧◡≦) ♡")
+        // 1L, 1L = Correr cada tick (0.05s).
+        ElytraTask(configLoader, visualManager).runTaskTimer(this, 0L, 1L)
+
+        logger.info("MyElytras activado con Mini-Engine nativo (≧◡≦) ♡")
     }
 
     override fun onDisable() {
+        if (::visualManager.isInitialized) {
+            visualManager.removeAll()
+        }
         logger.info("MyElytras desactivado.")
     }
-
-    fun getElytraLoader() = configLoader
 }
