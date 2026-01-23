91a2e524f56e4a5437738f4d35ea226972651266
ReDiego0 <re.diego.maldonado@gmail.com>
2026-01-22 22:18:15 -0300

    feat: Integrate ElytraConfigLoader and register core plugin components

--- src/main/kotlin/org/ReDiego0/myElytras/MyElytras.kt
+++ src/main/kotlin/org/ReDiego0/myElytras/MyElytras.kt
@@ -1,14 +1,28 @@
 package org.ReDiego0.myElytras
 
+import org.ReDiego0.myElytras.config.ElytraConfigLoader
+import org.ReDiego0.myElytras.listener.ElytraMechanicListener
+import org.ReDiego0.myElytras.task.ElytraTask
 import org.bukkit.plugin.java.JavaPlugin
 
 class MyElytras : JavaPlugin() {
+    private lateinit var configLoader: ElytraConfigLoader
 
     override fun onEnable() {
-        // Plugin startup logic
+        saveDefaultConfig()
+
+        configLoader = ElytraConfigLoader(this)
+        configLoader.load()
+
+        server.pluginManager.registerEvents(ElytraMechanicListener(configLoader), this)
+        ElytraTask(configLoader).runTaskTimer(this, 0L, 20L)
+
+        logger.info("MyElytras ha sido habilitado con éxito (≧◡≦) ♡")
     }
 
     override fun onDisable() {
-        // Plugin shutdown logic
+        logger.info("MyElytras desactivado.")
     }
+
+    fun getElytraLoader() = configLoader
 }
